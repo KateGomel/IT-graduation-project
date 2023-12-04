cc1bbf0efbae87a5a1e17e7fadc0f3b78c0cfff1
KateGomel <ykatuay@gmail.com>
2023-12-04 21:57:22 +0300

    add companent

--- main.js
+++ main.js
@@ -6,5 +6,6 @@ import "./src/components/typography/typography";
 import "./src/components/grid/grid";
 import "./src/components/input/input";
 import "./src/components/button/button";
+import "./src/components/image-product/image-product";
 
 import "./src/global/utils.scss";
