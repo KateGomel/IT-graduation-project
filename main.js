9a23b9d6d9a8ab4a26fe8657cdcce3e104f2f084
KateGomel <ykatuay@gmail.com>
2023-12-17 17:10:02 +0300

    add new sect

--- main.js
+++ main.js
@@ -11,5 +11,6 @@ import "./src/components/banner/banner";
 import "./src/components/mission/mission";
 import "./src/components/products/products";
 import "./src/components/product-card/product-card";
+import "./src/components/story/story";
 
 import "./src/global/utils.scss";
