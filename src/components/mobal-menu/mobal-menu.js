89f7f71e6305398866b1c81f9f57775fc2f81846
KateGomel <ykatuay@gmail.com>
2023-12-25 20:39:54 +0300

    add js mobal menu

--- src/components/mobal-menu/mobal-menu.js
+++ src/components/mobal-menu/mobal-menu.js
@@ -1,1 +1,12 @@
 import "./mobal-menu.scss";
+
+const openMenu = document.querySelector(".header__menu--mobal-img");
+const mobalMenu = document.querySelector(".mobal-menu");
+const closeMenu = document.querySelector(".mobal-menu__close");
+
+const toggleMenu = () => {
+  mobalMenu.classList.toggle("mobal-menu__active");
+};
+
+openMenu.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
