32471f70e565f563274f09faa0c1ca2a21ba65f9
KateGomel <ykatuay@gmail.com>
2023-12-27 14:05:12 +0300

    finish form  call-back

--- pages/call_back/call_back.js
+++ pages/call_back/call_back.js
@@ -1,6 +1,8 @@
 import "reset-css";
 
 import "../../src/components/typography/typography.js";
+import "../../src/components/button/button.js";
+import "../../src/components/input/input.js";
 import "../../src/components/call-back/call-back.js";
 
 import "../../main.scss";
